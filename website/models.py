f1d00867fac2edc278d6e754a71e92b7718dfb98
lucas123454653 <162929460+lucas123454653@users.noreply.github.com>
2025-07-22 16:29:40 +1200

    Likes Button Part 2
    Building a Like Feature for a Post

--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
 from . import db
+
 from sqlalchemy.sql import func
 from flask_login import UserMixin
 
@@ -13,23 +14,38 @@ class User(db.Model, UserMixin):
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True)
     comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
+
 
 
 
 # database model for blog post
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(150), unique=True)
+    title = db.Column(db.String(150), unique=False)
     content = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
     comments = db.relationship('Comment', backref='post', passive_deletes=True)
+    likes = db.relationship('Like', backref='post', passive_deletes=True)
 
 
-#database model for blog comment
+    
+    
+# database model for blog comment  
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200), unique=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
+    
+    
+
+
+# database model for comment like
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
--- website/views.py
+++ website/views.py
@@ -1,11 +1,14 @@
 from flask_login import login_user, logout_user, login_required, current_user
 
 # import external libraries
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
+
 #import database
 from . import db
+
 # import from .models user
-from .models import User, Post, Comment
+from .models import User, Post, Comment, Like
+
 # set views blueprint
 views = Blueprint("views", __name__)
 
@@ -104,6 +107,7 @@ def create_comment(post_id):
     return redirect(url_for("views.blog"))
 
 
+
 # delete comment post route
 @views.route("/delete-comment/<comment_id>")
 #user must be logged in to post
@@ -119,3 +123,24 @@ def delete_comment(comment_id):
         db.session.commit()
         flash('Comment deleted!', category='success')
     return redirect(url_for('views.blog'))
+
+
+
+# like comment route
+@views.route("/like-post/<post_id>", methods=['POST'])
+#user must be logged in to post
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    like = Like.query.filter_by(author=current_user.id, post=post_id).first
+    if not post:
+        return jsonify({'error': 'Post does not exist.'}, 400)
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else: 
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+
+    return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author, post.likes)})
