4e4399012b270958700ded74693a3035ce96e976
lucas123454653 <162929460+lucas123454653@users.noreply.github.com>
2025-07-22 18:36:26 +1200

    Fixing not being able to like error

--- website/views.py
+++ website/views.py
@@ -135,7 +135,7 @@ def delete_comment(comment_id):
 @login_required
 def like(post_id):
     post = Post.query.filter_by(id=post_id).first()
-    like = Like.query.filter_by(author=current_user.id, post=post_id).first()
+    like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
     if not post:
         return jsonify({'error': 'Post does not exist.'}, 400)
     elif like:
