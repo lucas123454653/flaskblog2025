d1dd4627ef35e55b51b6912d6572a3a656cceb66
lucas123454653 <162929460+lucas123454653@users.noreply.github.com>
2025-07-22 20:06:25 +1200

    Update Post Part 2 & fixing errors
    Creating a Dropdown Button and Update Function & fixing erros

--- website/forms.py
+++ website/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import current_user
+from flask_login import current_user
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, length, ValidationError
 from .models import User
