872cac07404340ee499d6b937af278fc95665372
lucas123454653 <162929460+lucas123454653@users.noreply.github.com>
2025-07-24 13:20:23 +1200

    Avatar Image/Account Page 1
    Avatar Image/Account Page 1

--- website/__init__.py
+++ website/__init__.py
@@ -29,6 +29,7 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+        print("Created Database!")
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
